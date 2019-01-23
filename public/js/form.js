32918f08dfd0e746dc3ecc5bde81c85b7ca90d10
Basir Baig <basir.baig81@gmail.com>
2019-01-23 17:12:18 +0500

    console logged result

--- public/js/form.js
+++ public/js/form.js
@@ -1,63 +1,82 @@
 var btn = document.getElementById("button");
-  btn.addEventListener("click", inputValues, makeRequest);
-  let serviceType = "";
-  let mailType = "";
-  let zipAddress1 = "";
-  let zipAddress2 = "";
-  let pounds = "";
-  let ounces = "";
-  let size = "";
-  let xmlData;
-  let xml;
-  let parseserviceType;
-  let parsemailType;
-  let parseZipAddress1;
-  let parseZipAddress2;
-  let parsePounds;
-  let parseOunces;
-  let parsesize;
-  let parsexmlData;
+btn.addEventListener("click", inputValues, makeRequest);
+let serviceType = "";
+let mailType = "";
+let zipAddress1 = "";
+let zipAddress2 = "";
+let pounds = "";
+let ounces = "";
+let size = "";
+let xmlData;
+let xml;
+let parseserviceType;
+let parsemailType;
+let parseZipAddress1;
+let parseZipAddress2;
+let parsePounds;
+let parseOunces;
+let parsesize;
+let parsexmlData;
 
-  function inputValues(e) {
-    serviceType = document.getElementById("InputServiceType").value;
-    mailType = document.getElementById("InputMailType").value;
-    zipAddress1 = document.getElementById("inputAddress").value;
-    zipAddress2 = document.getElementById("inputAddress2").value;
-    pounds = document.getElementById("inputPounds").value;
-    ounces = document.getElementById("inputOunces").value;
-    size = document.getElementById("inputSize").value;
+function inputValues(e) {
+  serviceType = document.getElementById("InputServiceType").value;
+  mailType = document.getElementById("InputMailType").value;
+  zipAddress1 = document.getElementById("inputAddress").value;
+  zipAddress2 = document.getElementById("inputAddress2").value;
+  pounds = document.getElementById("inputPounds").value;
+  ounces = document.getElementById("inputOunces").value;
+  size = document.getElementById("inputSize").value;
 
-    parseZipAddress1 = parseInt(zipAddress1);
-    parseZipAddress2 = parseInt(zipAddress2);
-    parsePounds = parseFloat(pounds);
-    parseOunces = parseFloat(ounces);
-    console.log(parseZipAddress1);
-    console.log(parseZipAddress2);
-    console.log(parsePounds);
-    console.log(parseOunces);
+  parseZipAddress1 = parseInt(zipAddress1);
+  parseZipAddress2 = parseInt(zipAddress2);
+  parsePounds = parseFloat(pounds);
+  parseOunces = parseFloat(ounces);
+  console.log(parseZipAddress1);
+  console.log(parseZipAddress2);
+  console.log(parsePounds);
+  console.log(parseOunces);
 
-    makeRequest();
+  makeRequest();
 
-    e.preventDefault();
-  }
+  e.preventDefault();
+}
 
+function makeRequest() {
+  const Http = new XMLHttpRequest();
+  //  const url = `http://production.shippingapis.com/ShippingAPI.dll?API=RateV4&XML=<RateV4Request USERID="945RADIO3150"><Revision>2</Revision><Package ID="1ST"><Service>"${serviceType}"</Service><FirstClassMailType>"${mailType}"</FirstClassMailType><ZipOrigination>${parseZipAddress1}</ZipOrigination><ZipDestination>${parseZipAddress2}</ZipDestination<Pounds>${parsePounds}</Pounds><Ounces>${parseOunces}</Ounces><Container/><Size>"${size}"</Size><Machinable>true</Machinable></Package></RateV4Request>`;
+  const url =
+    'http://production.shippingapis.com/ShippingAPI.dll?API=RateV4&XML=<RateV4Request USERID="945RADIO3150"><Revision>2</Revision><Package ID="1ST"><Service>FIRST CLASS</Service><FirstClassMailType>LETTER</FirstClassMailType><ZipOrigination>44106</ZipOrigination><ZipDestination>20770</ZipDestination><Pounds>0</Pounds><Ounces>3.12345678</Ounces><Container/><Size>REGULAR</Size><Machinable>true</Machinable></Package></RateV4Request>';
+  Http.open("GET", url);
+  Http.send();
+  Http.onreadystatechange = e => {
+    xmlData = Http.responseText;
 
-  function makeRequest() {
-    const Http = new XMLHttpRequest();
-    //  const url = `http://production.shippingapis.com/ShippingAPI.dll?API=RateV4&XML=<RateV4Request USERID="945RADIO3150"><Revision>2</Revision><Package ID="1ST"><Service>"${serviceType}"</Service><FirstClassMailType>"${mailType}"</FirstClassMailType><ZipOrigination>${parseZipAddress1}</ZipOrigination><ZipDestination>${parseZipAddress2}</ZipDestination<Pounds>${parsePounds}</Pounds><Ounces>${parseOunces}</Ounces><Container/><Size>"${size}"</Size><Machinable>true</Machinable></Package></RateV4Request>`;
-    const url =
-      'http://production.shippingapis.com/ShippingAPI.dll?API=RateV4&XML=<RateV4Request USERID="945RADIO3150"><Revision>2</Revision><Package ID="1ST"><Service>FIRST CLASS</Service><FirstClassMailType>LETTER</FirstClassMailType><ZipOrigination>44106</ZipOrigination><ZipDestination>20770</ZipDestination><Pounds>0</Pounds><Ounces>3.12345678</Ounces><Container/><Size>REGULAR</Size><Machinable>true</Machinable></Package></RateV4Request>';
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange = e => {
-      xmlData = Http.responseText;
+    parseXmlData(xmlData);
+  };
 
-      
+  var request = new XMLHttpRequest();
+
+  request.open(
+    "GET",
+    'http://production.shippingapis.com/ShippingAPI.dll?API=RateV4&XML=<RateV4Request USERID="945RADIO3150"><Revision>2</Revision><Package ID="1ST"><Service>FIRST CLASS</Service><FirstClassMailType>LETTER</FirstClassMailType><ZipOrigination>44106</ZipOrigination><ZipDestination>20770</ZipDestination><Pounds>0</Pounds><Ounces>3.12345678</Ounces><Container/><Size>REGULAR</Size><Machinable>true</Machinable></Package></RateV4Request>',
+    true
+  );
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      console.log(request.responseText);
+      xmlData = request.responseText;
       parseXmlData(xmlData);
-    };
-  }
-  function parseXmlData(x) {
-    let parser = new DOMParser();
-    xml = parser.parseFromString(x, "application/xml");
-    console.log(xml);
-  }
+    } else {
+      console.log("error");
+    }
+  };
+
+  request.send();
+}
+function parseXmlData(x) {
+  let parser = new DOMParser();
+  xml = parser.parseFromString(x, "application/xml");
+
+  console.log(xml);
+  console.log(xml.getElementsByTagName("Rate")[0].firstChild);
+}
